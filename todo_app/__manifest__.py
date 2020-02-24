a1d3a5fa730c85486f0118d7b21fafe3409777a6
Fabio Cardoso <fabio-992010@hotmail.com>
2020-02-24 15:44:49 +0100

    Subida de todo_app

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Fabio Cardoso',
+    'depends': ['mail'],
+    'data' : ['todo_view.xml'],
+    'application': True,
+}
