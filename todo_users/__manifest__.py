869b3c5c0bce122c60da7e291a8e91de8a6ff20f
Fabio Cardoso <fabio-992010@hotmail.com>
2020-02-24 16:06:52 +0100

    Subida del todo_user, es un extens de todo_app

---
+++ todo_users/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Multiuser To-Do',
+    'descripcion': 'Extend the To-Do app to multiuser',
+    'author': 'Fabio Cardoso',
+    'depends': ['todo_app'],
+    'data': ['todo_view.xml']
+}
